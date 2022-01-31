77abe83ae8e71cff158ca530aea972f883b7c386
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-01-30 20:25:26 -0600

    Changed text to print message with number of command line parameters present

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
-    cout << "Hello World" << endl;
-
+    cout << "My Command Line Analyzer.." << endl;
+    cout << "The number of command line parameters are: " << argc << endl;
 
     return 0;
 }
