8f87d48f3a9cafbd14638e34020cb0b4619fa109
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-01-31 15:22:21 -0600

    Added for loop to print message displaying the length of each entered parameter after displaying length of program name

--- main.cpp
+++ main.cpp
@@ -9,5 +9,9 @@ int main(int argc, char** argv) {
         cout << "The length of the name of the program is: " << string_length(argv[0]) << endl;
     }
 
+    for (int i=1; i < argc; i++){
+        cout << "The length of parameter " << i << " is: " << string_length(argv[i]) << endl;
+    }
+
     return 0;
 }
