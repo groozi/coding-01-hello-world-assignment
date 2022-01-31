fe1cab53ec84a9d5f0f18a27b55ace88f125e2af
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-01-31 14:41:13 -0600

    changed text to say 'length of name' rather than 'length of first parameter'

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
     cout << "The number of command line parameters are: " << argc << endl;
 
     if(argc > 1){
-        cout << "The length of the first parameter is " << string_length(argv[1]) << endl;
+        cout << "The length of the name of the program is: " << string_length(argv[0]) << endl;
     }
 
     return 0;
